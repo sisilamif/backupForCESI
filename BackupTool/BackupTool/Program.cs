2643004faefaf023765433ba868af22fc67e6ab5
sisilamif <106578829+sisilamif@users.noreply.github.com>
2024-01-30 15:32:48 +0100

    Add files via upload

---
+++ BackupTool/BackupTool/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Create source and destination directory
+        Console.WriteLine("Enter the source file path");
+        string ?sourceDirectory = Console.ReadLine();
+        Console.WriteLine("Enter destination file path");
+        string ?destinationDirectory = Console.ReadLine();
+        
+        // use the backupservice librairy
+        var backupService = new CreateBackup.BackupService();
+        backupService.PerformBackup(sourceDirectory, destinationDirectory);
+
+        Console.ReadLine();
+    }
+}
